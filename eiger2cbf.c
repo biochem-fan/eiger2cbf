cb81bd63d115b43b3c52153673eabe5ac8621956
keitaro yamashita <keitaroyam@users.noreply.github.com>
2016-05-10 11:53:56 +0900

    Fixed typo and more user-friendly message

--- eiger2cbf.c
+++ eiger2cbf.c
@@ -1,7 +1,7 @@
 /*
 EIGER HDF5 to CBF converter
  Written by Takanori Nakane
- (APPLY_FLATFILED_CORRECTION by Keitaro Yamashita)
+ (APPLY_FLATFIELD_CORRECTION by Keitaro Yamashita)
 
 To build:
 
@@ -44,7 +44,7 @@ gcc -std=c99 -o eiger2cbf -g \
 #include "hdf5_hl.h"
 
 // Comment out this line to skip flatfield correction.
-#define APPLY_FLATFILED_CORRECTION
+#define APPLY_FLATFIELD_CORRECTION
 
 extern const H5Z_class2_t H5Z_LZ4;
 extern const H5Z_class2_t bshuf_H5Filter;
@@ -234,7 +234,7 @@ int main(int argc, char **argv) {
   unsigned int *buf = (unsigned int*)malloc(sizeof(unsigned int) * xpixels * ypixels);
   signed int *buf_signed = (signed int*)malloc(sizeof(signed int) * xpixels * ypixels);
   signed int *pixel_mask = (signed int*)malloc(sizeof(signed int) * xpixels * ypixels);
-#ifdef APPLY_FLATFILED_CORRECTION
+#ifdef APPLY_FLATFIELD_CORRECTION
   float *flatfield = (float*)malloc(sizeof(float) * xpixels * ypixels);
   int flatfield_correction_applied;
   if (flatfield == NULL) {
@@ -274,10 +274,10 @@ int main(int argc, char **argv) {
     fprintf(stderr, " However, this might mask overloaded (saturated) pixels as well.\n");
   }
 
-#ifdef APPLY_FLATFILED_CORRECTION
+#ifdef APPLY_FLATFIELD_CORRECTION
   H5LTread_dataset_int(hdf, "/entry/instrument/detector/flatfield_correction_applied", &flatfield_correction_applied);
   if (flatfield_correction_applied == 1)
-    fprintf(stderr, "/entry/instrument/detector/flatfield_correction_applied is 1! skipping flatfiled correction");
+    fprintf(stderr, "/entry/instrument/detector/flatfield_correction_applied is 1! skipping flatfield correction.\n");
   else if (flatfield_correction_applied == 0) {
     flatfield[0] = 100.;
     H5LTread_dataset_float(hdf, "/entry/instrument/detector/detectorSpecific/flatfield", flatfield);
@@ -285,6 +285,7 @@ int main(int argc, char **argv) {
       fprintf(stderr, "WARNING: failed to read the flatfield from /entry/instrument/detector/detectorSpecific/flatfield.\n");
       return -1;
     }
+    fprintf(stderr, "/entry/instrument/detector/flatfield_correction_applied is 0! applying flatfield correction.\n");
   }
   else {
     fprintf(stderr, "/entry/instrument/detector/flatfield_correction_applied is neither 0 or 1!");
@@ -459,7 +460,7 @@ int main(int argc, char **argv) {
       } else if (pixel_mask[0] != -9999 && pixel_mask[i] > 1) { // the pixel mask is 2, 4, 8, 16
 	buf_signed[i] = -2;
       } else {
-#ifdef APPLY_FLATFILED_CORRECTION
+#ifdef APPLY_FLATFIELD_CORRECTION
 	buf_signed[i] = flatfield_correction_applied == 0 ? buf[i] * flatfield[i] + 0.5 : buf[i];
 #else
 	buf_signed[i] = buf[i];
